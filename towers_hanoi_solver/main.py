dcc8214db7893f2a419898c207f52d71d16e7aa6
randomforestkn <102574565+randomforestkn@users.noreply.github.com>
2022-03-29 21:34:09 +0300

    Create main.py

---
+++ towers_hanoi_solver/main.py
@@ -0,0 +1,14 @@
+def move(fr, to):
+    print(f"Move from {fr} to {to}")
+
+
+def foo(n, fr, to, spare):
+    if n == 1:
+        move(fr, to)
+    else:
+        foo(n-1, fr, spare, to)
+        foo(1, fr, to, spare)
+        foo(n-1, spare, to, fr)
+
+
+foo(64,"b","a","c")
